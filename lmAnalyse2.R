f6548395cb6a765795b23a84db7aa74764a5d420
PHRC <cordeiroph@gmail.com>
2018-12-12 08:32:21 +0000

    Analyse quantitative variables for linear regression

--- lmAnalyse2.R
+++ lmAnalyse2.R
@@ -1,187 +1,137 @@
 library(ggplot2)
 library(gridExtra)
+library(ggcorrplot)
 
-setwd("/Users/phrc/Documents/Projects/r projects/weather-prediction")
-df <- read.csv("Assignment 2.csv")
+#setwd("/Users/phrc/Documents/Projects/R projects/TemperaturePrediction")
+dfMaster <- read.csv("Assignment 2.csv")
+
+df <-dfMaster
+
+df$No <- NULL
+df$cbwd <- NULL
+df$year <- NULL
+df$month <- NULL
+df$day <- NULL
+df$hour <- NULL
 
 print(head(df))
 # Select only quantitative data
 print(summary(df))
 # pm2.5 DEWP TEMP PRESS IWS IS IR
 
-gPM <-  ggplot(na.omit(df), aes(x=pm2.5)) + 
-  boxplot(df$pm2.5, sub=paste("Outlier rows: ", boxplot.stats(df$pm2.5)$out)) +
-  labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$pm2.5)$out))
 
-print(gPM)
-
-gPM <- ggplot(na.omit(df), aes(x="", y=pm2.5)) +  
-  geom_boxplot(outlier.colour="black", outlier.shape=16,
-                     outlier.size=2, notch=FALSE) +
-  labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$pm2.5)$out))
-print(gPM)
-
-d <- boxplot.stats(df$pm2.5)
-d$stats
-d$n
-d$conf
-d$out
-
-grid.arrange( 
-  
-  gPM <- ggplot(na.omit(df), aes(x="", y=pm2.5)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$pm2.5)$out)),
-  
-  gPM <- ggplot(df, aes(x="", y=PRES)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$PRES)$out)),
-  
-  gPM <- ggplot(df, aes(x="", y=DEWP)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$DEWP)$out)),
-  
-  gPM <- ggplot(df, aes(x="", y=Iws)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$Iws)$out)),
-  
-  gPM <- ggplot(df, aes(x="", y=Is)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$Is)$out)),
-  
-  gPM <- ggplot(df, aes(x="", y=Ir)) +  
-    geom_boxplot(outlier.colour="black", outlier.shape=16,
-                 outlier.size=2, notch=FALSE) +
-    labs(subtitle=paste("Outlier rows: ", boxplot.stats(df$Ir)$out)),
-  
-  nrow = 3
-)
-
-grid.arrange( 
-
-  gPM <- ggplot(na.omit(df), aes(x=pm2.5, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gPRES <- ggplot(df, aes(x=PRES, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gDEWP <- ggplot(df, aes(x=DEWP, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIws <- ggplot(df, aes(x=Iws, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIs <- ggplot(df, aes(x=Is, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIr <- ggplot(df, aes(x=Ir, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  nrow = 3
-)
-
+outlierPlot <- function(df){
   grid.arrange( 
-    
-    gPM <- ggplot(na.omit(df), aes(x=pm2.5)) + 
-      geom_density() ,
-    
-    gPRES <- ggplot(df, aes(x=PRES)) + 
-      geom_density(),
-    
-    gDEWP <- ggplot(df, aes(x=DEWP)) + 
-      geom_density(),
-    
-    gIws <- ggplot(df, aes(x=Iws)) + 
-      geom_density(),
-    
-    gIs <- ggplot(df, aes(x=Is)) + 
-      geom_density(),
-    
-    gIr <- ggplot(df, aes(x=Ir)) + 
-      geom_density(),
-    
+    ggplot(df, aes(x="", y=pm2.5)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$pm2.5)$out))),
+    ggplot(df, aes(x="", y=PRES)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$PRES)$out))),
+    ggplot(df, aes(x="", y=DEWP)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$DEWP)$out))),
+    ggplot(df, aes(x="", y=Iws)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$Iws)$out))),
+    ggplot(df, aes(x="", y=Is)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$Is)$out))),
+    ggplot(df, aes(x="", y=Ir)) +  
+      geom_boxplot(outlier.colour="black", outlier.shape=16,
+                   outlier.size=2, notch=FALSE, na.rm = TRUE) +
+      labs(subtitle=paste("Outlier rows: ", length(boxplot.stats(df$Ir)$out))),
     nrow = 3
   )
+}
+
+densityPlot <- function(df){
+  grid.arrange( 
+    ggplot(na.omit(df), aes(x=pm2.5)) + 
+      geom_density(na.rm = TRUE),
+    ggplot(na.omit(df), aes(x=PRES)) + 
+      geom_density(na.rm = TRUE),
+    ggplot(na.omit(df), aes(x=DEWP)) + 
+      geom_density(na.rm = TRUE),
+    ggplot(na.omit(df), aes(x=Iws)) + 
+      geom_density(na.rm = TRUE),
+    ggplot(na.omit(df), aes(x=Is)) + 
+      geom_density(na.rm = TRUE),
+    ggplot(na.omit(df), aes(x=Ir)) + 
+      geom_density(na.rm = TRUE),
+    nrow = 3
+  )
+}
+
+linearPlot <- function(df){
+  grid.arrange(
+    ggplot(df, aes(x=pm2.5, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    ggplot(df, aes(x=PRES, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    ggplot(df, aes(x=DEWP, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    ggplot(df, aes(x=Iws, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    ggplot(df, aes(x=Is, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    ggplot(df, aes(x=Ir, y=TEMP)) + 
+      stat_binhex(na.rm = TRUE) +
+      geom_smooth(method="lm", na.rm = TRUE),
+    nrow = 3
+  )
+}
+
+correlationPlot <- function(df, corMethod){
+  corTab <- cor(dfNoOutLiers, use = "pairwise.complete.obs")
+  corTab[is.na(corTab)] = 0
+  ggcorrplot(corTab, hc.order = TRUE, 
+             type = "lower", 
+             lab = TRUE, 
+             lab_size = 4, 
+             method="square", 
+             ggtheme=theme_bw,
+             colors = c("red","white", "blue"))
+}
+
+outlierPlot(df)
+linearPlot(df)
+densityPlot(df)
+correlationPlot(df, "pearson")
+
+# Analyse without outliers -------
+
+dfNoOutLiers <- df
+
+boxStats <- boxplot.stats(df$pm2.5)$stats
+dfNoOutLiers[(!is.na(dfNoOutLiers$pm2.5) & (dfNoOutLiers$pm2.5 < boxStats[1] | dfNoOutLiers$pm2.5 > boxStats[5]) ),]$pm2.5 <- NA
+
+boxStats <- boxplot.stats(df$Iws)$stats
+dfNoOutLiers[(dfNoOutLiers$Iws < boxStats[1] |dfNoOutLiers$Iws > boxStats[5]),]$Iws <- NA 
+
+boxStats <- boxplot.stats(df$Is)$stats
+dfNoOutLiers[(dfNoOutLiers$Is < boxStats[1] | dfNoOutLiers$Is > boxStats[5]),]$Is <- NA 
+
+boxStats <- boxplot.stats(df$Ir)$stats
+dfNoOutLiers[(dfNoOutLiers$Ir < boxStats[1] | dfNoOutLiers$Ir > boxStats[5]),]$Ir <- NA 
+
+print(summary(dfNoOutLiers))
+
+outlierPlot(dfNoOutLiers)
+linearPlot(dfNoOutLiers)
+densityPlot(dfNoOutLiers)
+correlationPlot(dfNoOutLiers, "")
 
 
 
-dfN <- na.omit(df)
-boxplot.stats(dfN$DEWP)$stats
-dfOutlier <- dfN[(dfN$DEWP < -40 | dfN$DEWP > 28),]
 
-dfOutlier <- na.omit(dfOutlier)
-
-grid.arrange( 
-  
-  gPM <- ggplot(na.omit(dfOutlier), aes(x=pm2.5, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gPRES <- ggplot(dfOutlier, aes(x=PRES, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gDEWP <- ggplot(dfOutlier, aes(x=DEWP, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIws <- ggplot(dfOutlier, aes(x=Iws, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIs <- ggplot(dfOutlier, aes(x=Is, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  gIr <- ggplot(dfOutlier, aes(x=Ir, y=TEMP)) + 
-    stat_binhex() +
-    geom_smooth(method="lm"),
-  
-  nrow = 3
-)
-
-grid.arrange( 
-  
-  gPM <- ggplot(na.omit(dfOutlier), aes(x=pm2.5)) + 
-    geom_density() ,
-  
-  gPRES <- ggplot(dfOutlier, aes(x=PRES)) + 
-    geom_density(),
-  
-  gDEWP <- ggplot(dfOutlier, aes(x=DEWP)) + 
-    geom_density(),
-  
-  gIws <- ggplot(dfOutlier, aes(x=Iws)) + 
-    geom_density(),
-  
-  gIs <- ggplot(dfOutlier, aes(x=Is)) + 
-    geom_density(),
-  
-  gIr <- ggplot(dfOutlier, aes(x=Ir)) + 
-    geom_density(),
-  
-  nrow = 3
-)
-
-ggplot(dfOutlier, aes(x=DEWP)) + 
-  geom_density()
-
-ggplot(dfOutlier, aes(x=Iws)) + 
-  geom_density()
-
-ggplot(dfOutlier, aes(x=Is)) + 
-  geom_density()
-
-ggplot(dfOutlier, aes(x=Ir)) + 
-  geom_density()
